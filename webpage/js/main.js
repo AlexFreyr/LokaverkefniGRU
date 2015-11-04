aa62bfa7462fd8f1d8cf975917077dce95beb4ef
Alexander <alexfreyr98@gmail.com>
2015-11-04 17:30:31 +0000

    Virkar núna
    Lagaði kóðann þannig að núna virkar hann eins og planað var.

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -2,19 +2,27 @@
 $('#submit').on('click', function(){
     $('#notification').empty();
 
-    if($('#nafn').text() == ""){
-        $("#notification").fadeIn().append("Vantar nafn");
-    }else if($('#kennitala').text() == ""){
-        $("#notification").fadeIn().append("Vantar kennitölu");
-    }else if($('#netfang').text() == ""){
-        $("#notification").fadeIn().append("Vantar netfang");
-    }else if($('#land').text() == ""){
-        $("#notification").fadeIn().append("Vantar land");
-    }else if($('#lykilord').text() == ""){
-        $("#notification").fadeIn().append("Vantar lykilorð");
-    }else if($('#cb').is(':checked')){
-        // .. búið er að lesa skylmálann
+    var t = check("text");
+    var p = check("password");
+    var s = t + p;
+    if(s == 0){
+        if($('#cb').is(':checked')){
+            // .. continue
+        }else{
+            $("#notification").fadeIn().append("Þú þarft að lesa og samþykkja skilmálana áður en þú stofnar reikning");
+        }
     }else{
-        $("#notification").fadeIn().append("Vantar nafn");
+        $("#notification").fadeIn().append("Vantar að fylla út " + s + " reiti");
     }
 });
+
+function check(type) {
+    var empty = 0;
+    $('input[type=' + type + ']').each(function(){
+       if (this.value == "") {
+           empty++;
+           $("#error").show('slow');
+       } 
+    })
+    return empty;
+}
