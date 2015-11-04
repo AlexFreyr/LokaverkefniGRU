b0f6dd539a86fdc0e511b0aad73ff5ae49f2e310
Alexander <alexfreyr98@gmail.com>
2015-11-04 15:57:59 +0000

    Update
    Virkar á nyrAdgangur.html og setur inn texta þar sem á við. (Virkar ekki alveg ennþá)

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -1,1 +1,20 @@
+//Button click
+$('#submit').on('click', function(){
+    $('#notification').empty();
 
+    if($('#nafn').text() == ""){
+        $("#notification").fadeIn().append("Vantar nafn");
+    }else if($('#kennitala').text() == ""){
+        $("#notification").fadeIn().append("Vantar kennitölu");
+    }else if($('#netfang').text() == ""){
+        $("#notification").fadeIn().append("Vantar netfang");
+    }else if($('#land').text() == ""){
+        $("#notification").fadeIn().append("Vantar land");
+    }else if($('#lykilord').text() == ""){
+        $("#notification").fadeIn().append("Vantar lykilorð");
+    }else if($('#cb').is(':checked')){
+        // .. búið er að lesa skylmálann
+    }else{
+        $("#notification").fadeIn().append("Vantar nafn");
+    }
+});
