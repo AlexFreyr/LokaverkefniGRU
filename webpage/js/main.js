996532320265f147a73e2ff8ff1c734a421d66c8
Alexander <alexfreyr98@gmail.com>
2015-11-05 11:31:07 +0000

    Virkar betur
    Bætti við þannig að lykilorðið sem notandir setur inn þarf að vera amk 6 stafir og inniheldur 1 tölu, 1 stóran staf og 1 lítinn staf. Bætti líka við þannig að notandinn getur ekki ýtt á 'Submit' takann nema allar kröfur séu í lagi.

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -1,21 +1,46 @@
+var once = false;
 //Button click
-$('#submit').on('click', function(){
+$('#cb').on('click', function(){
     $('#notification').empty();
 
     var t = check("text");
     var p = check("password");
     var s = t + p;
     if(s == 0){
-        if($('#cb').is(':checked')){
-            // .. continue
+        if(checkLykilord($('#lykilord').val()) == true){
+            if($('#cb').is(':checked')){
+                $('button').prop('disabled', false); //Opnar takkann
+                once = true;
+            }else{
+                $("#notification").fadeIn().append("Þú þarft að lesa og samþykkja skilmálana áður en þú stofnar reikning");
+                $('button').prop('disabled', true);
+            }
         }else{
-            $("#notification").fadeIn().append("Þú þarft að lesa og samþykkja skilmálana áður en þú stofnar reikning");
+            $('button').prop('disabled', true);
+            $('#notification').fadeIn().append("Lykilorðið þarf að hafa amk 1 tölustaf, 1 stóran staf, 1 lítinn staf og 6 stafir langt");
         }
     }else{
-        $("#notification").fadeIn().append("Vantar að fylla út " + s + " reiti");
+        $('button').prop('disabled', true);
+        if (s == 1){
+            $("#notification").fadeIn().append("Vantar að fylla út " + s + " reit");    
+        }else{
+            $("#notification").fadeIn().append("Vantar að fylla út " + s + " reiti");
+        }
     }
 });
 
+$('#lykilord, #land, #netfang, #kennitala, #nafn').on('keydown', function(){
+    var t = check("text");
+    var p = check("password");
+    var s = t + p;
+    if(once == true && s == 0){
+        $('#cb').click();
+        $('#cb').prop('checked', false);
+        once = false;
+    }
+});
+
+//Gáir hvort það sé ekki örugglega texti í textboxunum.
 function check(type) {
     var empty = 0;
     $('input[type=' + type + ']').each(function(){
@@ -26,3 +51,17 @@ function check(type) {
     })
     return empty;
 }
+
+function checkLykilord(lykilord){
+    //Gáir hvort það sé amk. 1 tala, 1 stór stafur, 1 lítill stafur og amk 6 stafir
+    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    return re.test(lykilord);
+}
+
+$(document).ready(function() {
+     $('button').prop('disabled', true);
+ });
+
+/*
+Alexander Freyr
+*/
