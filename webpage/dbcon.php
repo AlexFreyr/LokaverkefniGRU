80bedeceee478f128197e44aa6643d500c1aa657
Michael Jes Þórsson <3108982369@TSNEM1150.2t.local>
2015-11-06 09:43:52 +0000

    Byrjun á tengingu við gagnagrunn
    Bætti við test síðu til þess að testa gagnagruna tengingar  og bætti smá
    við nyradgangur

---
+++ webpage/dbcon.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "tsuts.tskoli.is";
+$username = "2408982179";
+$password = "mypassword";
+$dbname = "2408982179_grulokaverk";
+
+try {
+    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$connection->exec('SET NAMES "utf8"');
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+?>
