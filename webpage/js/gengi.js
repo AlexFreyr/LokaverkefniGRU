c9656bc39819eba3e0f4a94e5bc837f9398f21a9
Alexander <alexfreyr98@gmail.com>
2015-11-12 11:30:01 +0000

    Gengisreikningar
    Náði að stytta kóðann fyrir 'gengi.js', og bætti við svo notandi geti
    bara notað tölustafi í 'input' boxunum. Þurfti reyndar að bæta við
    plug-in sem heytir numeric, bjó til nýja möppu í 'js' sem heitir plugins

--- webpage/js/gengi.js
+++ webpage/js/gengi.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 	var getCountry;
 	var getKaup = $("tr td:nth-child(2)");
@@ -7,41 +8,25 @@ $(document).ready(function() {
 		gengi[i] = 1000/getKaup[i].innerHTML;
 		$('#' + country[i]).val((Math.round(gengi[i] * 100)/100).toString());
 	}
+
+	$('input').numeric();
  });
 
-$('#isk').on('input', function(){
-	$('#usd').val((Math.round(($(this).val() / 129.37) * 100) / 100).toString());
-	$('#gbp').val((Math.round(($(this).val() / 197.34) * 100) / 100).toString());
-	$('#eur').val((Math.round(($(this).val() / 140.48) * 100) / 100).toString());
-	$('#jpy').val((Math.round(($(this).val() / 1.06) * 100) / 100).toString());
-});
+$('input').on('input', function(){
+	if($(this).attr("id") == "usd"){
+		$('#isk').val((Math.round(($(this).val() * 129.37) * 100) / 100).toString());
+	}else if($(this).attr("id") == "gbp"){
+		$('#isk').val((Math.round(($(this).val() * 197.34) * 100) / 100).toString());
+	}else if($(this).attr("id") == "eur"){
+		$('#isk').val((Math.round(($(this).val() * 140.48) * 100) / 100).toString());
+	}else if($(this).attr("id") == "jpy"){
+		$('#isk').val((Math.round(($(this).val() * 1.06) * 100) / 100).toString());
+	}
 
-$('#usd').on('input', function(){
-	$('#isk').val((Math.round(($(this).val() * 129.37) * 100) / 100).toString());
-	$('#gbp').val((Math.round(($('#isk').val() / 197.34) * 100) / 100).toString());
-	$('#eur').val((Math.round(($('#isk').val() / 140.48) * 100) / 100).toString());
-	$('#jpy').val((Math.round(($('#isk').val() / 1.06) * 100) / 100).toString());
-});
-
-$('#gbp').on('input', function(){
-	$('#isk').val((Math.round(($(this).val() * 197.34) * 100) / 100).toString());
-	$('#usd').val((Math.round(($('#isk').val() / 129.37) * 100) / 100).toString());
-	$('#eur').val((Math.round(($('#isk').val() / 140.48) * 100) / 100).toString());
-	$('#jpy').val((Math.round(($('#isk').val() / 1.06) * 100) / 100).toString());
-});
-
-$('#eur').on('input', function(){
-	$('#isk').val((Math.round(($(this).val() * 140.48) * 100) / 100).toString());
-	$('#usd').val((Math.round(($('#isk').val() / 129.37) * 100) / 100).toString());
-	$('#gbp').val((Math.round(($('#isk').val() / 197.34) * 100) / 100).toString());
-	$('#jpy').val((Math.round(($('#isk').val() / 1.06) * 100) / 100).toString());
-});
-
-$('#jpy').on('input', function(){
-	$('#isk').val((Math.round(($(this).val() * 1.06) * 100) / 100).toString());
 	$('#usd').val((Math.round(($('#isk').val() / 129.37) * 100) / 100).toString());
 	$('#gbp').val((Math.round(($('#isk').val() / 197.34) * 100) / 100).toString());
 	$('#eur').val((Math.round(($('#isk').val() / 140.48) * 100) / 100).toString());
+	$('#jpy').val((Math.round(($('#isk').val() / 1.06) * 100) / 100).toString());
 });
 
 /*
