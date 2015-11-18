96aa8438f0698efbf13e64806f6ac51f0bed566e
Alexander <alexfreyr98@gmail.com>
2015-11-18 23:55:15 +0000

    Authentication
    Virkar nokkurnveginn, þarf bara að gera svoleiðis að notandinn verði
    sendur af síðunni á aðra síðu

--- webpage/php/authentication.php
+++ webpage/php/authentication.php
@@ -1,23 +1,35 @@
-<?php
-	include "dbcon.php";
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Authenticating...</title>
+    <META charset="utf-8"/>
+</head>
+<body>
+    <?php
+        include "dbcon.php";
 
-	$kt = $_POST['login-kt'];
-	$lykill = $_POST['login-lykilord'];
+        $kt = $_POST['login-kt'];
+        $lykill = $_POST['login-lykilord'];
 
-    $command = "SELECT nafn FROM notandi WHERE kennitala = '$kt' AND lykilord = '$lykill'";
-    try
-    {
-        $result = $connection->query($command);
-    }
-    catch (PDOException $ex)
-    {
-        echo "Error fetching record: " . $ex->getMessage();
-    }
-    while($row = $result->fetch())
-	{
-	    $nafn[] = array($row['nafn']);
-	}
-	if ($result->num_rows == 0){
-		die("Wrong user/pass");
-	}else if ($result->)
-?>
+        $command = "SELECT nafn FROM notandi WHERE kennitala = '$kt' AND lykilord = '$lykill'";
+        try
+        {
+            $result = $connection->query($command);
+        }
+        catch (PDOException $ex)
+        {
+            echo "Error fetching record: " . $ex->getMessage();
+        }
+        while($row = $result->fetch())
+        {
+            $nafn[] = array($row['nafn']);
+        }
+
+        if(empty($nafn)){
+            echo "Array is empty";
+        }else{
+            echo $nafn[0][0];
+        }
+    ?>
+</body>
+</html>
