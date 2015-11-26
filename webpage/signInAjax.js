f29de3ea9cb9b430da73a1542d81613283ff67ab
Alexander <alexfreyr98@gmail.com>
2015-11-26 12:33:59 +0000

    Nokkrar breytingar
    Login formið virkar ekki núna, er að vinna í því. Bætti við auka checkum
    í userSignIn.php

--- webpage/signInAjax.js
+++ webpage/signInAjax.js
@@ -1,28 +1,43 @@
-$(document).ready(function(){
-	$('#login-submit').click(function(){
+function allow(){
+	var kt = $('#login-kt').val();
+	var password = $('#login-lykilord').val();
+	
+	if($.trim(kt) != '' && $.trim(password) != ''){
+		
+	}
+	return false;
+}
 
-		var kt = $('#login-kt').val();
-		var password = $('#login-lykilord').val();
+$(document).ready(function () {
+    $("#login-form").submit(function(e){
+    	e.preventDefault();
 
-		if($.trim(kt) != '' && $.trim(password) != ''){
-			
-			$.ajax({
-				type: 'POST',
-				url: 'ajax/userSignIn.php',
-				data: {
-					kt: kt,
-					password: password
-				},
-				success: function(data){
-					if(data == "Correct"){
-						window.location.replace('signed/user.php');
-					}
-					else{
-						$('#login-notification').text(data);
-					}
+    	$.ajax({
+			type: 'POST',
+			url: 'ajax/userSignIn.php',
+			data: {
+				kt: kt,
+				password: password
+			},
+			success: function(data){
+				if(data == "Success"){
+					
+					//window.location.replace('user/system.php');
 				}
-			});
+				else{
+					$('#login-notification').text(data);
+				}
+			}
+		});
+    });
+ });
+
+function showMsg(current, sender){
+	if(current == "Success"){
+		if(sender != null){
+			return false;
+		} else {
+			$('#login-form').unbind('submit').submit();
 		}
-		return false;
-	});
-});
+	}
+}
---
+++ webpage/user/system.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Áríon banki</title>
+	<meta charset="utf-8"/>
+</head>
+<body>
+	<?php
+		echo $_POST['kt'];
+	?>
+</body>
+</html>
