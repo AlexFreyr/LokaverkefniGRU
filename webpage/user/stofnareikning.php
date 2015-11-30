e490c00e70a6aa910932fb2cdbdf92e2fccdb1b7
Alexander <2408982179@TSNEM1354.2t.local>
2015-11-30 12:01:58 +0000

    Forms fyrir stofna reikning

--- webpage/user/stofnareikning.php
+++ webpage/user/stofnareikning.php
@@ -31,7 +31,7 @@
             </div>
             <div class="pure-u-1 pure-u-md-1-5">
                 <div class="pure-menu pure-menu-horizontal custom-can-transform">
-                    <ul class="pure-menu-list">
+                    <ul class="pure-menu-list"> 
                         <li class="pure-menu-item"><a href="home.php" class="pure-menu-link">Reikningar</a></li>
                         <li class="pure-menu-item"><a href="millifaera.php" class="pure-menu-link">Millifæra</a></li>
                         <li class="pure-menu-item"><a href="#" class="pure-menu-link" id="selected">Stofna reikning</a></li>
@@ -64,6 +64,58 @@
         <div class="main">
             <h2 class="middle-style">Stofna reikning</h2>
 
+            <form class="pure-form pure-form-stacked">
+                <legend>Upplýsingar um nýjann reikning</legend>
+                <div class="pure-g">
+                    <fieldset>
+                        <div class="pure-u-1 pure-u-md-1-3">
+                            <label for="first-name">First Name</label>
+                            <input id="first-name" class="pure-u-23-24" type="text">
+                        </div>
+
+                        <div class="pure-u-1 pure-u-md-1-3">
+                            <label for="last-name">Last Name</label>
+                            <input id="last-name" class="pure-u-23-24" type="text">
+                        </div>
+
+                        <div class="pure-u-1 pure-u-md-1-3">
+                            <label for="email">E-Mail</label>
+                            <input id="email" class="pure-u-23-24" type="email" required>
+                        </div>
+
+                        <div class="pure-u-1 pure-u-md-1-3">
+                            <label for="city">City</label>
+                            <input id="city" class="pure-u-23-24" type="text">
+                        </div>
+
+                        <div class="pure-u-1 pure-u-md-1-3">
+                            <label for="tegund">Tegund reiknings</label>
+                            <select id="tegund" class="pure-input-1">
+                                <option>Sparnaðarreikningur</option>
+                                <option>Ávísunarreikningur</option>
+                                <option>Debitkortareikingur</option>
+                                <option>Kreditkortareikingur</option>
+                                <option>Framtíðarreikningur</option>
+                            </select>
+                        </div>
+
+                        <div class="pure-u-1 pure-u-md-1-3">
+                            <label for="vextir">Vextir</label>
+                            <select id="vextir" class="pure-input-1-2">
+                                <option>1.5%</option>
+                                <option>2.1%</option>
+                                <option>2.8%</option>
+                                <option>3.7%</option>
+                                <option>4.8%</option>
+                            </select>
+                        </div>
+
+                        <div class="pure-controls">
+                            <button type="button" class="pure-button pure-button-primary" id="klara-btn">Klára</button>
+                        </div>
+                    </fieldset>
+                </div>
+            </form>
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <!--Öll scripts fara fyrir neðan þetta comment-->
